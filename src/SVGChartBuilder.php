5e2991d1d78ec39fde8b1fcdd3cb9e196eeb38ae
juan.penaverdu <juan.penaverdu@plexus.es>
2023-08-23 10:02:13 +0200

    Initial boilerplate

---
+++ src/SVGChartBuilder.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xanpena\SVGChartBuilder;
+
+class SVGChartBuilder
+{
+
+    /*
+    |--------------------------------------------------------------------------
+    | SVGChartBuilder
+    |--------------------------------------------------------------------------
+    |
+    | This is the main class of the SVGChartBuilder that will allow you to
+    | generate pretty charts from backend.
+    |
+    */
+
+    public function __construct()
+    {
+    }
+
+}
